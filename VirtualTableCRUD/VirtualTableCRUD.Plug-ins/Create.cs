7d8cd505cd78aa6445a88e5d0ecbf582daa6a4e0
Danilo Capuano <capuanodanilo@outlook.com>
2021-04-08 18:09:43 +0200

    Add files via upload

---
+++ VirtualTableCRUD/VirtualTableCRUD.Plug-ins/Create.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Extensions;
+using Microsoft.Xrm.Sdk.Data.Exceptions;
+using Newtonsoft.Json;
+
+namespace VirtualTableCRUD.Plug_ins
+{
+    public class Create : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.Get<IPluginExecutionContext>();
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+                Guid id = Guid.NewGuid();
+                string cmdString = "INSERT INTO person (PersonId,FirstName,LastName,Email) VALUES (@PersonId,@FirstName,@LastName,@Email)";
+                SqlConnection connection = Connection.GetConnection();
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = cmdString;
+                    command.Parameters.AddWithValue("@PersonId", id);
+                    command.Parameters.AddWithValue("@FirstName", entity["dc_firstname"]);
+                    command.Parameters.AddWithValue("@LastName", entity["dc_lastname"]);
+                    command.Parameters.AddWithValue("@Email", entity["dc_email"]);
+                    connection.Open();
+                    try
+                    {
+                        var numRecords = command.ExecuteNonQuery();
+                        Console.WriteLine("inserted {0} records", numRecords);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+                context.OutputParameters["id"] = id;
+            }
+        }
+    }
+}
---
+++ VirtualTableCRUD/VirtualTableCRUD.Plug-ins/RetrieveMultiple.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Extensions;
+using Microsoft.Xrm.Sdk.Data.Exceptions;
+using Newtonsoft.Json;
+
+namespace VirtualTableCRUD.Plug_ins
+{
+    public class RetrieveMultiple : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.Get<IPluginExecutionContext>();
+            EntityCollection collection = new EntityCollection();
+            string cmdString = "SELECT PersonId, FirstName, LastName, Email FROM person";
+            SqlConnection connection = Connection.GetConnection();
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = cmdString;
+                connection.Open();
+                try
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Entity e = new Entity("dc_person");
+                            e.Attributes.Add("dc_personid", reader.GetGuid(0));
+                            e.Attributes.Add("dc_firstname", reader.GetString(1));
+                            e.Attributes.Add("dc_lastname", reader.GetString(2));
+                            e.Attributes.Add("dc_email", reader.GetString(3));
+                            collection.Entities.Add(e);
+                        }
+                    }
+                }
+                finally
+                {
+                    connection.Close();
+                }
+                context.OutputParameters["BusinessEntityCollection"] = collection;
+            }
+        }
+    }
+}
