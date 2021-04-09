d0b8f376f7464602ba118026260f38d78d541761
Danilo Capuano <danilo.capuano@agictech.com>
2021-04-09 14:17:34 +0200

    Initial Commit

---
+++ VirtualTableCRUD/VirtualTableCRUD.Plug-ins/Delete.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Extensions;
+using Microsoft.Xrm.Sdk.Data.Exceptions;
+using Newtonsoft.Json;
+
+namespace VirtualTableCRUD.Plug_ins
+{
+    public class Delete : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.Get<IPluginExecutionContext>();
+            Guid id = Guid.Empty;
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is EntityReference)
+            {
+                EntityReference entityRef = (EntityReference)context.InputParameters["Target"];
+                id = entityRef.Id;
+                string cmdString = "DELETE person WHERE PersonId=@PersonId";
+                SqlConnection connection = Connection.GetConnection();
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = cmdString; command.Parameters.AddWithValue("@PersonId", id);
+                    connection.Open();
+                    try
+                    {
+                        var numRecords = command.ExecuteNonQuery();
+                        Console.WriteLine("deleted {0} records", numRecords);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+
+    }
+}
